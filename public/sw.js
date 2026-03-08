1d67032f11c7060dc02d701f115497d8226acc92
cavalieridelcodice <cavalieridelcodice@gmail.com>
2026-03-08 01:28:20 +0100

    aggiornamento favicon del 8-03-2026 ore 01:28

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+// Simple Service Worker for PWA installability
+const CACHE_NAME = 'kash-v1';
+const ASSETS_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/site.webmanifest',
+    '/kash-icon.png',
+    '/web-app-manifest-192x192.png',
+    '/web-app-manifest-512x512.png',
+    '/apple-touch-icon.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => response || fetch(event.request))
+    );
+});
